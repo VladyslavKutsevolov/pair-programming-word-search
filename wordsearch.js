383e222675ca5bc6c4bc3829353bffb39eedae91
Vladyslav <kytsevolov@gmail.com>
2020-08-26 17:42:35 -0600

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require('./transpose');
 
-module.exports = wordSearch
+const findWord = (array, word) => {
+  const horizontalJoin = array.map((ls) => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const wordSearch = (letters, word) => {
+  const flippedArray = transpose(letters);
+
+  return findWord(letters, word) || findWord(flippedArray, word);
+};
+
+module.exports = wordSearch;
